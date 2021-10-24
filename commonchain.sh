f9f32fd3e8f299f0f944680a09b66fd74f077979
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-10-24 20:33:10 -0300

    Switch from Windows to Linux; working  and (nov) variables; improve the 'latest' cmd; other updates and fixes

--- commonchain.sh
+++ commonchain.sh
@@ -1,35 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 
-nm="CommonChain"
-vs="r1"
-commonchainversion="""
-        \e[1;42m $nm \e[0m\e[100m version $vs \e[0m
-        \e[100m Created in 10-21-2021 by Daniella Mesquita \e[0m
-"""
-
-lastblock=$(find -maxdepth 1 -name '*.json' | sort -t_ -nk2,2 | tail -n1)
-prevblock="This feature isn't available yet."
-gitrepo=$(jq -r '.gitrepo' 0.json)
-releasetag=$(curl --silent ""https://api.github.com/repos/\"$gitrepo\""/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-releasetagnov=$(curl --silent ""https://api.github.com/repos/\"$gitrepo\""/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | cut -c 2-)
-
-if [ "$1" = "" ]; then
-   echo "$commonchainversion"
-   echo "Type 'commonchain --help' (without quotes) to see a list of available commands."
-   echo "'gitrepo': $gitrepo" #test
-   echo "'releasetag': $releasetag" #test
-   echo "'releasetagnov': $releasetagnov" #test
-fi
-
-if [ "$1" = "latest" ]; then
-   echo "Latest block is:"
-   echo "$lastblock"
-   if [ "$2" = "validate" ]; then
-      echo "This feature isn't available yet."
-fi
-fi
-
-if [ "$1" = "previous" ]; then
-   echo "Previous block is:"
-   echo "$prevblock"
-fi
+##!/bin/sh
+##!/bin/bash -e

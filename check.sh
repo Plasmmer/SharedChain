7c479a1c17c90cbeba440d1520c35052f5683117
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-10-25 02:55:59 -0300

    fix identation in validate.sh and check.sh

--- check.sh
+++ check.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -e
 
-      tmp="$(($lastblock + 1))"
-      #echo -n >$tmp.json
-      cat > $tmp.json << ENDOFFILE
+tmp="$(($lastblock + 1))"
+cat > $tmp.json << ENDOFFILE
 {
         "prevblock": "",
 	"tag": "",
@@ -10,20 +9,20 @@
 	"ipfs": ""
 }
 ENDOFFILE
-      tmp2="$(ipfs add -q --only-hash ./$lastblock.json)"
-      contents="$(jq ".prevblock = \"$tmp2\"" $tmp.json)" && \
-      echo "${contents}" > $tmp.json
-      tmp2=""
-      contents="$(jq ".tag = \"$releasetag\"" $tmp.json)" && \
-      echo "${contents}" > $tmp.json
-      filename=$(jq -r '.fileprefix' 0.json)$releasetag$(jq -r '.filesufix' 0.json)
-      freshurl="https://github.com/"$gitrepo"/releases/download/"$releasetag"/"$filename
-      contents="$(jq ".url = \"$freshurl\"" $tmp.json)" && \
-      echo "${contents}" > $tmp.json
-      wget -q $freshurl
-      tmp2="$(ipfs add -q --only-hash ./$filename)"
-      contents="$(jq ".ipfs = \"$tmp2\"" $tmp.json)" && \
-      echo "${contents}" > $tmp.json
-      rm $filename
-      tmp2="" && filename="" && freshurl="" && tmp=""
-      echo -n >justupdated.now
+tmp2="$(ipfs add -q --only-hash ./$lastblock.json)"
+contents="$(jq ".prevblock = \"$tmp2\"" $tmp.json)" && \
+echo "${contents}" > $tmp.json
+tmp2=""
+contents="$(jq ".tag = \"$releasetag\"" $tmp.json)" && \
+echo "${contents}" > $tmp.json
+filename=$(jq -r '.fileprefix' 0.json)$releasetag$(jq -r '.filesufix' 0.json)
+freshurl="https://github.com/"$gitrepo"/releases/download/"$releasetag"/"$filename
+contents="$(jq ".url = \"$freshurl\"" $tmp.json)" && \
+echo "${contents}" > $tmp.json
+wget -q $freshurl
+tmp2="$(ipfs add -q --only-hash ./$filename)"
+contents="$(jq ".ipfs = \"$tmp2\"" $tmp.json)" && \
+echo "${contents}" > $tmp.json
+rm $filename
+tmp2="" && filename="" && freshurl="" && tmp=""
+echo -n >justupdated.now
--- validate.sh
+++ validate.sh
@@ -1,51 +1,51 @@
 #!/bin/bash -e
 
-      echo "Validating..."
-      echo "[1/4] Fresh URL vs. latest block's URL:"
-      filename=$(jq -r '.fileprefix' 0.json)$lastblocktag$(jq -r '.filesufix' 0.json)
-      freshurl="https://github.com/"$gitrepo"/releases/download/"$lastblocktag"/"$filename
-      echo "$lastblocktag"
-      if [ "$(jq -r '.url' $lastblock.json)" = "$freshurl" ]; then
+echo "Validating..."
+echo "[1/4] Fresh URL vs. latest block's URL:"
+filename=$(jq -r '.fileprefix' 0.json)$lastblocktag$(jq -r '.filesufix' 0.json)
+freshurl="https://github.com/"$gitrepo"/releases/download/"$lastblocktag"/"$filename
+echo "$lastblocktag"
+if [ "$(jq -r '.url' $lastblock.json)" = "$freshurl" ]; then
+   echo "Done! There's a match!"
+   echo "Validation 1/4 passed."
+   echo "[2/4] latest block's URL vs. IPFS CID:"
+   echo "Downloading release file from Fresh URL..."
+   wget $freshurl
+   echo "Checking for package integrity..."
+#   if [ "$(ipfs add -q --only-hash ./$filename | ipfs cid base32)" = "$(jq -r '.ipfs' $lastblock.json)" ]
+   if [ "$(ipfs add -q --only-hash ./$filename)" = "$(jq -r '.ipfs' $lastblock.json)" ]
+      then
+         echo "CID/Hash is the same from requested and the downloaded file, so the download is ok."
+         #tar -xzf $filename
          echo "Done! There's a match!"
-         echo "Validation 1/4 passed."
-         echo "[2/4] latest block's URL vs. IPFS CID:"
-         echo "Downloading release file from Fresh URL..."
-         wget $freshurl
-         echo "Checking for package integrity..."
-#         if [ "$(ipfs add -q --only-hash ./$filename | ipfs cid base32)" = "$(jq -r '.ipfs' $lastblock.json)" ]
-         if [ "$(ipfs add -q --only-hash ./$filename)" = "$(jq -r '.ipfs' $lastblock.json)" ]
+         echo "Validation 2/4 passed."
+         echo "The latest block seems valid! But, next step..."
+         echo "[3/4] Validating first block with genesis block:"
+         if [ "$(ipfs add -q --only-hash ./1.json)" = "$(jq -r '.firstblock' 0.json)" ]
             then
-               echo "CID/Hash is the same from requested and the downloaded file, so the download is ok."
-               #tar -xzf $filename
                echo "Done! There's a match!"
-               echo "Validation 2/4 passed."
-               echo "The latest block seems valid! But, next step..."
-               echo "[3/4] Validating first block with genesis block:"
-               if [ "$(ipfs add -q --only-hash ./1.json)" = "$(jq -r '.firstblock' 0.json)" ]
+               echo "Validation 3/4 passed."
+               echo "[4/4] Previous block's hash:"
+               if [ "$lastblock" -ge "2" ]
                   then
-                     echo "Done! There's a match!"
-                     echo "Validation 3/4 passed."
-                     echo "[4/4] Previous block's hash:"
-                     if [ "$lastblock" -ge "2" ]
+                     if [ "$(ipfs add -q --only-hash ./$prevblock.json)" = "$(jq -r '.prevblock' $lastblock.json)" ]
                         then
-                           if [ "$(ipfs add -q --only-hash ./$prevblock.json)" = "$(jq -r '.prevblock' $lastblock.json)" ]
-                           then
-                              echo "Done! There's a match!"
-                              echo "Validation 4/4 passed. Traversing into all previous blocks will be implemented futurely."
-                           else
-                              echo "Requested CID/Hash $(jq -r '.prevblock' $lastblock.json) is different from the result: $(ipfs add -q --only-hash ./$prevblock.json)."
-                              echo "Validation 4/4 failed. This commonchain is corrupted in the last, previous or more blocks. Please search a way to fix."
-fi
+                           echo "Done! There's a match!"
+                           echo "Validation 4/4 passed. Traversing into all previous blocks will be implemented futurely."
                         else
-                           echo "Validation 4/4 passed. Can't verify previous block because this commonchain is still at block 1. Wait until it reach block 2 or more."
+                           echo "Requested CID/Hash $(jq -r '.prevblock' $lastblock.json) is different from the result: $(ipfs add -q --only-hash ./$prevblock.json)."
+                           echo "Validation 4/4 failed. This commonchain is corrupted in the last, previous or more blocks. Please search a way to fix."
 fi
                   else
-                     echo "Requested CID/Hash $(jq -r '.firstblock' 0.json) is different from the result: $(ipfs add -q --only-hash ./1.json)."
-                     echo "Validation 3/4 failed. This commonchain is corrupted. Please search a way to fix."
+                     echo "Validation 4/4 passed. Can't verify previous block because this commonchain is still at block 1. Wait until it reach block 2 or more."
 fi
             else
-               echo "Requested CID/Hash $(jq -r '.ipfs' $lastblock.json) is different from the result: $(ipfs add -q --only-hash ./$filename). Your download is corrupt. Please try again."
-               echo "Validation 2/4 failed."
+               echo "Requested CID/Hash $(jq -r '.firstblock' 0.json) is different from the result: $(ipfs add -q --only-hash ./1.json)."
+               echo "Validation 3/4 failed. This commonchain is corrupted. Please search a way to fix."
 fi
-         rm $filename
+      else
+         echo "Requested CID/Hash $(jq -r '.ipfs' $lastblock.json) is different from the result: $(ipfs add -q --only-hash ./$filename). Your download is corrupt. Please try again."
+         echo "Validation 2/4 failed."
+fi
+rm $filename
 fi

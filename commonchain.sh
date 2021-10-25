a5ec0e966dd7676f5359ffe206f049ee491259fb
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-10-25 00:29:29 -0300

    Validate (3/4) if first block matches in genesis block

--- commonchain.sh
+++ commonchain.sh
@@ -27,15 +27,6 @@ releasetagnov="$(
   | cut -c 2-
 )"
 
-#WORDTOREMOVE1=".json"
-#tmp="$(echo $lastblock | sed s/"$WORDTOREMOVE1"//)"
-#WORDTOREMOVE2="./"
-#echo $tmp | sed s/"$WORDTOREMOVE2"//
-
-#echo $lastblock | sed '/^#/,/^\.json/{/^#/!{/^\.json/!d}}'
-
-#echo $lastblock | sed '1,/firstmatch/d;/.json/,$d'
-
 if [ "$1" = "" ]; then
    echo "$commonchainversion"
    echo "Type 'commonchain --help' (without quotes) to see a list of available commands."
@@ -51,15 +42,14 @@ if [ "$1" = "latest" ]; then
       echo "$lastblock"
 fi
    if [ "$2" = "validate" ]; then
-      echo "This feature isn't available yet. Or is?"
       echo "Validating..."
-      echo "[1/3] Fresh URL vs. latest block's URL:"
+      echo "[1/4] Fresh URL vs. latest block's URL:"
       filename=$(jq -r '.fileprefix' 0.json)$releasetag$(jq -r '.filesufix' 0.json)
       freshurl="https://github.com/"$gitrepo"/releases/download/"$releasetag"/"$filename
       if [ "$(jq -r '.url' $lastblock.json)" = "$freshurl" ]; then
          echo "Done! There's a match!"
-         echo "Validation 1/3 passed."
-         echo "[2/3] latest block's URL vs. IPFS CID:"
+         echo "Validation 1/4 passed."
+         echo "[2/4] latest block's URL vs. IPFS CID:"
          echo "Downloading release file from Fresh URL..."
          wget $freshurl
          echo "Checking for package integrity..."
@@ -69,31 +59,39 @@ fi
                echo "CID/Hash is the same from requested and the downloaded file, so the download is ok."
                #tar -xzf $filename
                echo "Done! There's a match!"
-               echo "Validation 2/3 passed."
+               echo "Validation 2/4 passed."
                echo "The latest block seems valid! But, next step..."
-               echo "[2/3] Previous block's hash:"
-               if [ "$lastblock" -ge "2" ]
+               echo "[3/4] Validating first block with genesis block:"
+               if [ "$(ipfs add -q --only-hash ./1.json)" = "$(jq -r '.firstblock' 0.json)" ]
                   then
-                     if [ "$(ipfs add -q --only-hash ./$prevblock.json)" = "$(jq -r '.prevblock' $lastblock.json)" ]
-                     then
-                        echo "Done! There's a match!"
-                        echo "Validation 3/3 passed. Traversing into all previous blocks will be implemented futurely."
-                     else
-                        echo "Requested CID/Hash $(jq -r '.prevblock' $lastblock.json) is different from the result: $(ipfs add -q --only-hash ./$prevblock.json)."
-                        echo "Validation 3/3 failed. This commonchain is corrupt in the last, previous or more blocks. Please search a way to fix."
+                     echo "Done! There's a match!"
+                     echo "Validation 3/4 passed."
+                     echo "[4/4] Previous block's hash:"
+                     if [ "$lastblock" -ge "2" ]
+                        then
+                           if [ "$(ipfs add -q --only-hash ./$prevblock.json)" = "$(jq -r '.prevblock' $lastblock.json)" ]
+                           then
+                              echo "Done! There's a match!"
+                              echo "Validation 4/4 passed. Traversing into all previous blocks will be implemented futurely."
+                           else
+                              echo "Requested CID/Hash $(jq -r '.prevblock' $lastblock.json) is different from the result: $(ipfs add -q --only-hash ./$prevblock.json)."
+                              echo "Validation 4/4 failed. This commonchain is corrupted in the last, previous or more blocks. Please search a way to fix."
+fi
+                        else
+                           echo "Validation 4/4 passed. Can't verify previous block because this commonchain is still at block 1. Wait until it reach block 2 or more."
 fi
                   else
-                     echo "Validation 2/3 passed. Can't verify previous block because this commonchain is still at block 1. Wait until it reach block 2 or more."
+                     echo "Requested CID/Hash $(jq -r '.firstblock' 0.json) is different from the result: $(ipfs add -q --only-hash ./1.json)."
+                     echo "Validation 3/4 failed. This commonchain is corrupted. Please search a way to fix."
 fi
             else
                echo "Requested CID/Hash $(jq -r '.ipfs' $lastblock.json) is different from the result: $(ipfs add -q --only-hash ./$filename). Your download is corrupt. Please try again."
-               echo "Validation 2/3 failed."
+               echo "Validation 2/4 failed."
 fi
          rm $filename
 fi
 fi
 fi
-
 
 
 if [ "$1" = "previous" ]; then
